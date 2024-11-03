98be3cc0b10767205633c9fef18a87c651c103b8
Joseph Scott <joseph@josephscott.org>
2024-11-02 22:29:28 -0600

    More tests

--- tests/get-tests.php
+++ tests/get-tests.php
@@ -8,3 +8,56 @@ test( 'get-curl', function () {
 	expect( $response->error )->toBe( false );
 	expect( $response->code )->toBe( 200 );
 } );
+
+test( 'get-404-response', function () {
+	$request = new \T7\HTTP\Request();
+	$response = $request->get( url: 'http://localhost:17171/not-found' );
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+} );
+
+test( 'get-with-query-parameters', function () {
+	$request = new \T7\HTTP\Request();
+	$response = $request->get(
+		url: 'http://localhost:17171/echo?name=test&value=123'
+	);
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+	$body = json_decode( $response->body, true );
+	expect( $body )->toHaveKey( 'get' );
+	expect( $body['get']['name'] )->toBe( 'test' );
+	expect( $body['get']['value'] )->toBe( '123' );
+} );
+
+test( 'get-with-custom-headers', function () {
+	$request = new \T7\HTTP\Request();
+	$response = $request->get(
+		url: 'http://localhost:17171/headers',
+		headers: ['X-Custom-Header' => 'test-value']
+	);
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+	expect( $response->body )->toContain( 'X-Custom-Header' );
+} );
+
+test( 'get-invalid-url', function () {
+	$request = new \T7\HTTP\Request();
+	$response = $request->get( url: 'http://invalid-domain-that-does-not-exist.test' );
+
+	expect( $response->error )->toBe( true );
+	expect( $response->code )->toBe( 0 );
+} );
+
+test( 'get-timeout', function () {
+	$request = new \T7\HTTP\Request();
+	$response = $request->get(
+		url: 'http://localhost:17171/delay',
+		options: ['timeout' => 1]
+	);
+
+	expect( $response->error )->toBe( false );
+	expect( $response->code )->toBe( 200 );
+} );
